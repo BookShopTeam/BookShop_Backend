788202ee66efbe2269c351e0dc860007653bf8aa
thangle98 <leanhthang10a1@gmail.com>
2020-09-25 18:38:55 +0700

    HoanThanhTuongtacmodel_index

--- Shop Ban Sach/ShopQuocViet/Controllers/DanhMucSPController.cs
+++ Shop Ban Sach/ShopQuocViet/Controllers/DanhMucSPController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopQuocViet.Models;
 
 namespace ShopQuocViet.Controllers
 {
     public class DanhMucSPController : Controller
     {
+        BookShopEntities1 db = new BookShopEntities1();
         // GET: DanhMucSP
         public ActionResult Index()
         {
-            return View();
+            var listDanhMuc = from DM in db.DanhMuc
+                              select DM;
+            return View(listDanhMuc.ToList());
         }
+        public ActionResult PartialChuDe(string id)
+        {
+
+            var listChuDe = from CD in db.ChuDe
+                            where CD.MaDM == id
+                            select CD;
+            return PartialView(listChuDe.ToList());
+        }
+
     }
 }
--- Shop Ban Sach/ShopQuocViet/Controllers/HomeController.cs
+++ Shop Ban Sach/ShopQuocViet/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShopQuocViet.Models;
 
 namespace ShopQuocViet.Controllers
 {
     public class HomeController : Controller
     {
+        BookShopEntities1 db = new BookShopEntities1();
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult PartialSachNoiBat()
+        {
+            var listSachNoiBat = db.Sach.Take(8).ToList();
+            return PartialView(listSachNoiBat);
+        }
+        public ActionResult PartialQuanTam()
+        {
+            var listSachQuanTam = db.Sach.Take(16).ToList();
+            return PartialView(listSachQuanTam);
+        }
+
     }
 }
